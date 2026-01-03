a33054abe302740c00ea83df6a980d5be1476cfb
Ted J Burgess <ted.j.burgess@gmail.com>
2026-01-03 12:04:39 +0100

    Created some classes
    Created
    -Taskcontroller
    -TestRunner
    -Idgenerator
    
    Test runner is just to test CRUD logic. It will be removed at the end of the project

--- src/main/java/com/example/taskmanager/Task.java
+++ src/main/java/com/example/taskmanager/Task.java
@@ -1,11 +1,25 @@
 package com.example.taskmanager;
 
 public class Task {
-    long id;
+    int id;
     String title;
-    Boolean completed;
+    Boolean status;
+    private static final IdGenerator generatedId = new IdGenerator();
 
-    public Task() {
-        
+    public Task(String title) {
+        this.title = title;
+        this.id = generatedId.next();
+        this.status = false;
     }
+
+    public String testGetInfo() {
+        return ("ID: " + id + "\n" +
+            "Title: " + title + "\n" +
+            "Status: " + status + "\n"
+        );        
+    }
+
+
+
+    
 }
