28f32dee2ab58c917d5f99cab547628f6a43e895
Ted J Burgess <ted.j.burgess@gmail.com>
2026-01-05 16:23:09 +0100

    Delte task functionality DONE

--- src/main/java/com/example/taskmanager/TaskController.java
+++ src/main/java/com/example/taskmanager/TaskController.java
@@ -1,11 +1,16 @@
 package com.example.taskmanager;
 
 import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 
@@ -17,22 +22,44 @@ public class TaskController {
     }
 
     @PostMapping("/tasks")
+    @ResponseStatus(HttpStatus.CREATED)
     public Task createTask(@RequestBody String title) {
         return store.createTask(title);
     }
 
     @GetMapping("/tasks")
     public List<Task> getTasks() {
-        return store.getTasks();
+        List<Task> list = store.getTasks();
+        if (list.size() == 0) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+        return list;
     }
 
     @GetMapping("/tasks/{ID}")
     public Task getTask(@PathVariable int ID) {
-        return store.getTask(ID);
+        Task task = store.getTask(ID);
+        if (task == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+        return task;
     }
 
     @PostMapping("/tasks/{ID}")
     public Task taskDone(@PathVariable int ID) {
-        return store.markCompleted(ID);
+        Task task = store.markCompleted(ID);
+        if (task == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+        return task;
+    }
+
+    @DeleteMapping("/tasks/{ID}")
+    public List<Task> deleteTask(@PathVariable int ID) {
+        List<Task> list = store.deleteTask(ID);
+        if (list == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+        return list;
     }
 }
--- src/main/java/com/example/taskmanager/TestRunner.java
+++
@@ -1,9 +0,0 @@
-package com.example.taskmanager;
-
-public class TestRunner {
-    public static void main(String[] args) {
-        
-    }
-
-
-}
